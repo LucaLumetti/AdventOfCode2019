eadf7151a94a87af7a8a52afaa2c74fd4c16a63f
Luca Lumetti <lumetti.luca@gmail.com>
2019-12-04 22:14:15 +0100

    remove useless part for d4p2js

--- javascript/day04/part2.js
+++ javascript/day04/part2.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 let range = fs.readFileSync('input', 'utf-8').trim().split('-').map(Number)
 
 function meetCriteria(password){
-  password = `${password}`
-  let r1 = password.match(/(\d)\1+(?!\1)/g)
-  if(r1 == null)
-    return false
-  if(r1.filter((v)=>v.length == 2).length==0)
-    return false
-  if(password.split('').sort().join('') != password)
-    return false
-  return true
+    password = `${password}`
+    let r1 = password.match(/(\d)\1+/g)
+    if(r1 == null)
+        return false
+    if(r1.filter((v)=>v.length == 2).length==0)
+        return false
+    if(password.split('').sort().join('') != password)
+        return false
+    return true
 }
 
 console.log((new Array(range[1]-range[0])).fill(0).map((v, i) => i+range[0]).filter(meetCriteria).length)
