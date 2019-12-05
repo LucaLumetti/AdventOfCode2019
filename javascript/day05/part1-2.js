92b2a85be5dd101ddb6355da261e075b452b7079
Luca Lumetti <lumetti.luca@gmail.com>
2019-12-05 12:38:12 +0100

    day05 js

---
+++ javascript/day05/part1-2.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+let input = fs.readFileSync('input', 'utf-8').trim()
+
+// opcode: number of params
+const OPCODE = {
+    '99': 0,
+    '01': 3,
+    '02': 3,
+    '03': 1,
+    '04': 1,
+    '05': 2,
+    '06': 2,
+    '07': 3,
+    '08': 3
+}
+
+function execute (memory){
+        memory = memory.map(Number)
+    for(let i = 0; i < memory.length;){
+        memory[i] = `0000${memory[i]}`
+        let opcode = memory[i].substr(memory[i].length-2,memory[i].length)
+        let mode = memory[i].substr(0,memory[i].length-2).split('')
+
+        memory[i] = Number(memory[i])
+        mode = mode.reverse()
+        mode.push(0)
+        mode.push(0)
+        mode.push(0)
+
+        let first = mode[0] == 1 ? i+1 : memory[i+1]
+        let second = mode[1] == 1 ? i+2 : memory[i+2]
+        let third = mode[2] == 1 ? i+3 : memory[i+3]
+
+        i+=OPCODE[opcode]+1
+        if(opcode == 99)
+            break
+
+        if(opcode == 1)
+            memory[third] = memory[first] + memory[second]
+        if(opcode == 2)
+            memory[third] = memory[first] * memory[second]
+        if(opcode == 3)
+            memory[first] = 1
+        if(opcode == 4)
+            console.log("OUT: " + memory[first])
+        if(opcode == 5)
+            i = memory[first]?memory[second]:i
+        if(opcode == 6)
+            i = !memory[first]?memory[second]:i
+        if(opcode == 7)
+            memory[third] = memory[first]<memory[second]?1:0
+        if(opcode == 8)
+            memory[third] = memory[first]==memory[second]?1:0
+    }
+
+    return memory[0]
+}
+
+execute(input.split(','))
